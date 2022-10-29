8d8735fa0e3bf1ba3bdde68a90ed0f38d0cdd087
Jeevan P <jeevan.puthukka@eeg.tech>
2022-10-29 15:34:59 +0530

    main: initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Profile from "./containers/profile/Profile";
+import { GlobalStyle } from "./global.style";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Profile />
+    </>
+  );
+};
+
+export default App;
